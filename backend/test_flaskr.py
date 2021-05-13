f11df3768e518a2c67e732a6b400e745aa5e5c87
Elena <elena.luzuriagac@gmail.com>
2021-05-13 16:15:16 -0500

    New changes

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,8 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
-#to test the delete function you have to change this parameter before runing the test
+# to test the delete function you have to change this parameter before
+# runing the test
 delete_id = 32
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -17,7 +19,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = 'postgresql://postgres:admin@localhost:5432/' + self.database_name #"postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = 'postgresql://postgres:admin@localhost:5432/' + \
+            self.database_name  # "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -27,28 +30,42 @@ class TriviaTestCase(unittest.TestCase):
             # create all tables
             self.db.create_all()
 
-        self.new_question= {
+        self.new_question = {
             'question': 'What is the only state in the United States that does not have a flag in a shape with 4 edges?',
-            'answer': 'Ohio', 
+            'answer': 'Ohio',
             'difficulty': 3,
-            'category': 3
-        }
-    
+            'category': 3}
+
     def tearDown(self):
         """Executed after reach test"""
         pass
-    #test to get  the questions
+    
+    #test to get categories
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertTrue(data['categories'])
+    
+    #test get categories - failure scenario
+    def test_405_get_categories(self):
+        res = self.client().post('/categories')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
+    
+    # test to get  the questions
     def test_get_paginated_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
 
-        #self.assertEqual(res.status.code,200)
         self.assertTrue(data['questions'])
         self.assertTrue(data['total_questions'])
-        #self.assertTrue(len(data['questions']))
         self.assertTrue(data['categories'])
 
-
+    # test get questions - failure scenario
     def test_404_sent_requesting_beyond_valid_page(self):
         res = self.client().get('/questions?page=1000')
         data = json.loads(res.data)
@@ -57,7 +74,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
 
-    #create a new question
+    # create a new question
     def test_create_new_question(self):
         res = self.client().post('/questions', json=self.new_question)
         data = json.loads(res.data)
@@ -65,9 +82,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['created'])
-    '''    
+
+    def test_405_create_new_question(self):
+        res = self.client().post('/questions/50', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')
+
+    
     # Remember to change the parameter delete_id before running the test
-    def test_delete_question(self):
+    '''def test_delete_question(self):
         res = self.client().delete('/questions/' + str(delete_id))
         data = json.loads(res.data)
 
@@ -76,27 +102,68 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['deleted'],2)'''
-    
+
+    def test_404_if_questions_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')    
+
     # Test to search a question
     def test_search_question(self):
         self.search_term = {'searchTerm': 'Tom'}
         res = self.client().post('/questions/search', json=self.search_term)
         data = json.loads(res.data)
-        
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['questions'])
     
+    # Test to search a question - failure scenario
+    def test_422_wrong_request_search_question(self):
+        self.search_term = {'searchTerm': 'Tom'}
+        res = self.client().get('/questions/search', json=self.search_term)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')  
+    
     # Test to play
     def test_questions_quizzes(self):
-        
-        res = self.client().post('/quizzes', json={'previous_questions':['5'], 'quiz_category':{"type": "Geography", "id": 3}},)
+
+        res = self.client().post(
+            '/quizzes',
+            json={
+                'previous_questions': ['5'],
+                'quiz_category': {
+                    "type": "Geography",
+                    "id": 3}},
+        )
         data = json.loads(res.data)
-        
+
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
-    
+
+    # Test to play
+    def test_questions_quizzes(self):
+
+        res = self.client().get(
+            '/quizzes',
+            json={
+                'previous_questions': ['5'],
+                'quiz_category': {
+                    "type": "Geography",
+                    "id": 3}},
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'method not allowed')  
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
